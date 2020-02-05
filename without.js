ae10af4d66800f9277e3deed68cc7ce2f49d6c62
victorcwyu <victorcwyu@gmail.com>
2020-02-05 14:59:33 +0000

    Creation of a function which will remove unwanted elements from an array and return a new array

---
+++ without.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴 🔴 🔴  Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function(source,itemsToRemove) {
+  const resort = source.filter((source) => !itemsToRemove.includes(source));
+  return resort;
+};
