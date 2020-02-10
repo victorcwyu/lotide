268c7338b14b43ab44f4afe7ba85ac02c74d1aac
victorcwyu <victorcwyu@gmail.com>
2020-02-10 22:34:39 +0000

    Commented out test code

--- flatten.js
+++ flatten.js
@@ -51,5 +51,5 @@ const flatten = function(array) {
 // };
 
 
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+// console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
 // arrays = [1, 2, [3, 4], 5, [6]] // => [1, 2, 3, 4, 5, 6]
