e135c7a9e61e1895653be30dcfdfa0119b914ecb
victorcwyu <victorcwyu@gmail.com>
2020-02-10 20:59:51 +0000

    Updated tail function

--- tail.js
+++ tail.js
@@ -1,20 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 🔴 🔴  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// A SECOND SOLUTION, INVOLVING EXTRA VARIABLE AND A FOR LOOP
+// const arrayEquate = function (actual, expected) {
+//   for (let i = 0; i < actual.length; i++) {
+//     assertEqual(actual[i], expected[i]);
+//   }
+// };
 
-const arrayEquate = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    assertEqual(actual[i], expected[i]);
-  }
-};
-
-const tail = function(array) {
+const tail = function (array) {
   return array.slice(1);
 };
 
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-arrayEquate(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
