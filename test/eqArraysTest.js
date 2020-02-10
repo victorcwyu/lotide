8c7d9d4473cc316a8ce055116ab58a82bfac719f
victorcwyu <victorcwyu@gmail.com>
2020-02-10 22:10:38 +0000

    Fixed typo

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-describe("#tail", () => {
+describe("#eqArrays", () => {
   it("returns true for ([1, 2, 3], [1, 2, 3])", () => {
     assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
