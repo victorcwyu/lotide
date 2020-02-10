e07cc95eb9c8f3445cab85018638722c8b87da41
victorcwyu <victorcwyu@gmail.com>
2020-02-10 21:21:12 +0000

    Updated eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 🔴 🔴  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (arr1,arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -17,14 +9,4 @@ const eqArrays = function (arr1,arr2) {
   } return true;
 };
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
