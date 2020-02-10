3cfa030fefad223e2f6e47a9db6b9ff03e8c8edc
victorcwyu <victorcwyu@gmail.com>
2020-02-10 20:45:56 +0000

    Created test modules for assert and head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("blah", "blah");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(420, 69);
+assertEqual(420, "420");
